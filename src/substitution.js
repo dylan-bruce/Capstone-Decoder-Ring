0e552e0b0d03fa8d7dc4a2b4e24a32b0f9c33388
Dylan Bruce <dyanb1127@gmail.com>
2022-01-01 18:45:24 -0500

    Completed substitution problem

--- src/substitution.js
+++ src/substitution.js
@@ -5,9 +5,33 @@
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
+  // https://theasciicode.com.ar/ for ASCII codes.
 
   function substitution(input, alphabet, encode = true) {
     // your solution code here
+    let message = ""; //Return message
+    let lowerInput = input.toLowerCase(); //Setting input to lowercase
+    if(!alphabet || alphabet.length != 26) return false; //Check if alphabet exists and is 26 characters
+    const only = {}; //Storage object
+    for(letter in alphabet.split('')){ //If each letter is not in storage object, store and check next letter
+      const uniqueletter = alphabet[letter]; //If a matching letter comes up, return false
+      if(only[uniqueletter]) return false; // If unique letter is in only, return false
+      else only[uniqueletter] = true; // If unique letter is not in only, store it
+    }
+    if(encode){ //When encode is true
+      for(let i=0; i<lowerInput.length; i++){ //Loop through input
+        const asciiCode = lowerInput.charCodeAt(i);  //Getting ASCII code for each letter
+        if(lowerInput[i] === ' ') message += ' '; //If character is a space, store a space
+        else message += alphabet[asciiCode - 97]; //If character is a letter, store ASCII code - 97
+      }
+    } else if (!encode){ //When encode is false (decode)
+      for(let i=0; i<lowerInput.length; i++){ //Loop through input
+        const indexNumber = alphabet.split('').findIndex(letter => letter === lowerInput[i]); //get index number for each letter
+        if(lowerInput[i] === ' ') message += ' '; //If character is a space, store a space
+        else message += String.fromCharCode(indexNumber + 97); //If character is an letter, use index to get ASCII code for letter
+      }
+    }
+    return message; //Return message;
   }
 
   return {
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,31 @@
 // Write your tests here!
+const { expect } = require("chai");
+const { substitution } = require("../src/substitution");
+
+describe('substitution() written by Dylan', () => {
+    it('Should return false if alphabet is less than 26 letters', () => {
+        const expected = false;
+        const actual = substitution('Hello World!', 'abc');
+        expect(actual).to.equal(expected);
+    });
+    it('Should return false if alphabet is greater than 26 letters', () => {
+        const expected = false;
+        const actual = substitution('Hello World!', 'aabbccddeeffgghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzz');
+        expect(actual).to.equal(expected);
+    });
+    it('Should return false if alphabet doesnt exist', () => {
+        const expected = false;
+        const actual = substitution('Hello World!');
+        expect(actual).to.equal(expected);
+    });
+    it('Should encode with special characters', () => {
+        const expected = 'jgnnq yqtnf';
+        const actual = substitution('Hello World', '#$efghijklmnopqrstuvwxyz!@');
+        expect(actual).to.equal(expected);
+    });
+    it('Should decode with special characters', () => {
+        const expected = 'hello world';
+        const actual = substitution('jgnnq yqtnf', '#$efghijklmnopqrstuvwxyz!@', false);
+        expect(actual).to.equal(expected);
+    });
+});
